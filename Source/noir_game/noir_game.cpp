fb8806856734c48b52015ae9b012281a81fc2b2b
DazyLude <36658009+DazyLude@users.noreply.github.com>
2022-09-16 09:21:58 +0400

    initial commit

---
+++ Source/noir_game/noir_game.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "noir_game.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, noir_game, "noir_game" );
---
+++ Source/noir_game/noir_gameGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "noir_gameGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NOIR_GAME_API Anoir_gameGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
